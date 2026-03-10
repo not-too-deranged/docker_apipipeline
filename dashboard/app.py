459132795bfd821254cd5ff0c67c0d230faaa835
jamie <mine.maus99@freenet.de>
2026-03-10 21:12:02 +0100

    added test query

--- dashboard/app.py
+++ dashboard/app.py
@@ -5,3 +5,15 @@ import psycopg2
 st.title("Global Data Dashboard")
 
 st.write("API Data Visualization")
+
+from backend.queries import get_crypto_data
+
+st.title("Global Data Dashboard")
+
+data = get_crypto_data()
+
+df = pd.DataFrame(data, columns=["id", "price", "timestamp"])
+
+st.subheader("Bitcoin Price")
+
+st.line_chart(df["price"])
